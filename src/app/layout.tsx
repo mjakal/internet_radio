6b3c2662d4cb43c62bde79201cd415fac0a41de5
Tomislav <tomislav.oreskovic.125@gmail.com>
2025-05-10 21:26:52 +0200

    moved animations.css import to MarqueeText

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { Outfit } from 'next/font/google';
 import './globals.css';
-import './animations.css';
 
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
--- src/components/common/MarqueeText.tsx
+++ src/components/common/MarqueeText.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import '../../app/animations.css';
 
 interface MarqueeTextProps {
   text: string;
