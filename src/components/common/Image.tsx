f996fba3f6dccdd03693f70a92de16e40383f62f
mjakal <martin.jakal@telsmart.eu>
2025-05-27 23:48:41 +0200

    station favicon bug fix

--- src/components/common/Image.tsx
+++ src/components/common/Image.tsx
@@ -1,27 +1,51 @@
-import { useState } from 'react';
-import Image, { ImageProps } from 'next/image';
+import { useEffect, useCallback, useState } from 'react';
+import Image from 'next/image';
 
-interface ImageWithFallbackProps extends ImageProps {
+interface ImageWithFallbackProps {
+  src: string;
   fallbackSrc: string;
+  className?: string;
+  alt: string;
+  width: number;
+  height: number;
 }
 
 const ImageWithFallback: React.FC<ImageWithFallbackProps> = ({
   src,
   fallbackSrc,
+  className,
   alt,
-  ...props
+  width,
+  height,
 }) => {
-  const [imgSrc, setImgSrc] = useState<string>(src && typeof src === 'string' ? src : fallbackSrc);
+  const [imgSrc, setImgSrc] = useState<string>(src ? src : fallbackSrc);
 
-  const handleError = () => {
+  useEffect(() => {
+    setImgSrc((prevState) => {
+      if (prevState === src) return prevState;
+
+      return src;
+    });
+  }, [src]);
+
+  const handleError = useCallback(() => {
     setImgSrc((prevState) => {
       if (prevState === fallbackSrc) return prevState;
 
       return fallbackSrc;
     });
-  };
+  }, [fallbackSrc]);
 
-  return <Image {...props} src={imgSrc} alt={alt} onError={handleError} />;
+  return (
+    <Image
+      src={imgSrc}
+      className={className}
+      alt={alt}
+      width={width}
+      height={height}
+      onError={handleError}
+    />
+  );
 };
 
 export default ImageWithFallback;
