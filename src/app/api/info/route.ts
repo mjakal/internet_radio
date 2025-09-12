788e4653ad0f34ffde039af04d0e1c2effe63071
mjakal <mjakal@gmail.com>
2025-09-12 17:05:01 +0200

    double url decoding bug fix

--- src/app/api/info/route.ts
+++ src/app/api/info/route.ts
@@ -1,14 +1,13 @@
 import { NextResponse } from 'next/server';
 import { getStreamInfo } from '../../../lib/icy';
-// here is my backend function that receives url how to decode it
+
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
-    const encodedUrl = searchParams.get('stream') || '';
+    const streamUrl = searchParams.get('stream') || '';
 
-    if (!encodedUrl) return NextResponse.json({ nowPlaying: '' });
+    if (!streamUrl) return NextResponse.json({ nowPlaying: '' });
 
-    const streamUrl = decodeURIComponent(encodedUrl);
     const nowPlaying = await getStreamInfo(streamUrl);
 
     return NextResponse.json({ nowPlaying });
